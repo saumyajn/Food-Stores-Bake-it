0f9e7ff3ceced85582b57e4d81792a991f557bab
saumi06 <saumyajn1994@gmail.com>
2020-07-01 03:19:32 -0400

    use of Controller and MVC pattern

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController');
 // Do work here
-router.get('/', (req, res) => {
-  const data={ name:'Saumya',age:26, cool:true};
-  // res.send('Hey! It works!');
-  // res.json(data);
-  res.render('hello',{
-    name:'sanket',
-    dog:req.query.dog,
-    title:'Bake It!'
-  });
-});
+router.get('/',storeController.homePage)
 
 router.get('/reverse/:name',(req,res) => {
   const reverse = [...req.params.name].reverse().join('');
