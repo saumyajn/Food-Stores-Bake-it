8100def3b9f7db99c49de9bfc272299ba5acf888
saumi06 <saumyajn1994@gmail.com>
2020-07-01 17:02:38 -0400

    Add Storeform using mixins

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,13 +1,21 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name= 'Saums';
-  // if(req.name =='Saums'){
-  //   throw Error('That is a nice name!');
-  // } to create error
-  // res.cookie('name','Saumya is cool!',{maxAge:90000}); //To set cookies
-  next();
+// exports.myMiddleware = (req, res, next) => {
+//   req.name= 'Saums';
+//   // if(req.name =='Saums'){
+//   //   throw Error('That is a nice name!');
+//   // } to create error
+//   // res.cookie('name','Saumya is cool!',{maxAge:90000}); //To set cookies :happy
+//   next();
 
-}
+// }
 exports.homePage = (req, res) => {
 console.log(req.name);
   res.render('index');
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore',{title:'Add Store'});
+} 
+
+exports.createStore = (req,res) => {
+  res.json(req.body);
 }
---
+++ models/Store.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slugs: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+
+});
+
+storeSchema.pre('save', function (next) {
+  if(!this.isModified('name')){
+    return next();
+  }
+  this.slug = slug(this.name);
+  next();
+});
+module.exports = mongoose.model('Store', storeSchema);
