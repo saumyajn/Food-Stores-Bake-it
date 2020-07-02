1637ede15133faf70fe925a4d93f684ea1defa5a
saumi06 <saumyajn1994@gmail.com>
2020-07-02 18:56:07 -0400

    add image

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer= require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req,file, next){
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto){
+      next(null,true);
+    } else {
+      next({message: 'That file type isn\'t allowed.'}, false);
+    }
+  }
+}
 
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Bake Stores' });
@@ -8,6 +23,23 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async  (req, res, next) => {
+  //check if there is no new file to resize
+  if(!req.file){
+    next();
+    return;//skip to next middleware
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  //now resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next(); 
+}
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
@@ -48,4 +80,10 @@ exports.updateStore = async (req, res) => {
   res.redirect(`/stores/${store._id}/edit`);
   //redirect them to store and tell the user
   
+}
+exports.getStoreBySlug = async (req,res, next) => {
+  const store = await Store.findOne({slug: req.params.slug});
+  if(!store) return next();
+  //redirect to pug page
+  res.render('showStore', { title: `${store.name}`, store});
 }
--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,17 @@ router.get('/stores', catchErrors(storeController.getStores));
 
 router.get('/add', storeController.addStore);
 
-router.post('/add', catchErrors(storeController.createStore));
+router.post('/add', 
+storeController.upload,
+catchErrors(storeController.resize), 
+catchErrors(storeController.createStore));
 
 router.get('/stores/:id/edit',catchErrors(storeController.editStore));
 
-router.post('/add/:id', catchErrors(storeController.updateStore));
+router.post('/add/:id', 
+storeController.upload,
+catchErrors(storeController.resize), catchErrors(storeController.updateStore));
 
+router.get('/stores/:slug',
+catchErrors(storeController.getStoreBySlug))
 module.exports = router;
